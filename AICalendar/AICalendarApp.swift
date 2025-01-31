87b85f1dfc14b3153b2851480e180a3ef12f00cb
Kacper Gajko <kacper.gajko1@icloud.com>
2025-01-31 01:12:13 +0100

    Initial Commit

---
+++ AICalendar/AICalendarApp.swift
@@ -0,0 +1,32 @@
+//
+//  AICalendarApp.swift
+//  AICalendar
+//
+//  Created by Kacper on 31/01/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AICalendarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AICalendarTests/AICalendarTests.swift
@@ -0,0 +1,16 @@
+//
+//  AICalendarTests.swift
+//  AICalendarTests
+//
+//  Created by Kacper on 31/01/2025.
+//
+
+import Testing
+
+struct AICalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
