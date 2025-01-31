09db2af16bfed7c839474cf6d3cc4b926062e518
Kacper Gajko <kacper.gajko1@icloud.com>
2025-01-31 17:59:25 +0100

    display all sidebars with ForEach

--- AICalendar/Views/Sidebar.swift
+++ AICalendar/Views/Sidebar.swift
@@ -13,31 +13,40 @@ struct Sidebar: View{
     @State private var showText = false
     @State var sidebarWidth: CGFloat = 55
     
+    @State var tabs: [SidebarButton] = [
+        SidebarButton(view: .calendar, icon: "calendar", text: "Calendar"),
+        SidebarButton(view: .settings, icon: "gear", text: "Settings")
+    ]
+    
     var body: some View{
         VStack(){
             if !isHovering{
-                HStack(){
-                    Image(systemName: "calendar")
-                        .font(.system(size: 28))
-                        .padding(.leading, 10)
-                        .padding(.vertical)
-                }
-                .frame( maxWidth: .infinity, alignment: .leading)
-            } else {
-                Button(action: {
-            
-                }){
-                    HStack(alignment: .center){
-                        Image(systemName: "calendar")
+                ForEach(tabs, id: \.view){ tab in
+                    HStack(){
+                        Image(systemName: tab.icon)
                             .font(.system(size: 28))
                             .padding(.leading, 10)
                             .padding(.vertical)
-                        Text("Calendar")
                     }
                     .frame( maxWidth: .infinity, alignment: .leading)
-                    .contentShape(Rectangle())
                 }
-                .buttonStyle(.plain)
+            } else {
+                ForEach(tabs, id: \.view){ tab in
+                    Button(action: {
+                        currentView = tab.view
+                    }){
+                        HStack(alignment: .center){
+                            Image(systemName: tab.icon)
+                                .font(.system(size: 28))
+                                .padding(.leading, 10)
+                                .padding(.vertical)
+                            Text(tab.text)
+                        }
+                        .frame( maxWidth: .infinity, alignment: .leading)
+                        .contentShape(Rectangle())
+                    }
+                    .buttonStyle(.plain)
+                }
             }
             Spacer()
         }
