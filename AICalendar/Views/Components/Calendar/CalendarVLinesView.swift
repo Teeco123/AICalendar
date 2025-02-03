ea23d9a1327a437fafc7a3a807688287effbb32e
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-03 17:46:02 +0100

    move evets with VLines

--- AICalendar/Views/Components/Calendar/CalendarVLinesView.swift
+++ AICalendar/Views/Components/Calendar/CalendarVLinesView.swift
@@ -12,25 +12,18 @@ struct CalendarVLinesView: View{
     var dayWidth: CGFloat
     
     var body: some View{
-        HStack(spacing: 0){
-            Rectangle()
-                .fill(.black.opacity(0))
-                .frame(width: hourLabel.width + 10, height: .infinity, alignment: .trailing)
-            ScrollView(.horizontal){
-                HStack(spacing: 0) {
-                    ForEach(0...20, id: \.self) { index in
-                        VStack(spacing: 10) {
-                            Rectangle()
-                                .fill(.gray.opacity(0.6))
-                                .frame(width: 1, height: .infinity)
-                        }
-                        .frame(width: dayWidth)
-                        .foregroundColor(.gray)
-                    }
+        HStack(spacing: 0) {
+            ForEach(0...20, id: \.self) { index in
+                VStack(spacing: 10) {
+                    Rectangle()
+                        .fill(.gray.opacity(0.6))
+                        .frame(width: 1, height: .infinity)
                 }
-                .offset(y: hourLabel.height - 20)
+                .frame(width: dayWidth)
+                .foregroundColor(.gray)
             }
         }
+        .offset(y: hourLabel.height - 20)
     }
 }
 
--- AICalendar/Views/Components/Calendar/CalendarView.swift
+++ AICalendar/Views/Components/Calendar/CalendarView.swift
@@ -29,25 +29,31 @@ struct CalendarView: View {
                     use24HourFormat: use24HourFormat,
                     hourLabel: hourLabel
                 )
-                CalendarVLinesView(
-                    hourLabel: hourLabel,
-                    dayWidth: dayWidth
-                )
-                ForEach(eventViewModel.processedEvents, id: \.self){ events in
-                    HStack(){
-                        ForEach(events){ event in
-                            CalendarEventView(
-                                event: event,
-                                startHour: startHour,
-                                endHour: endHour,
-                                calendarHeight: calendarHeight,
+                HStack(spacing: 0){
+                    Rectangle()
+                        .fill(.black.opacity(0))
+                        .frame(width: hourLabel.width + 10, height: .infinity, alignment: .trailing)
+                    ScrollView(.horizontal){
+                        ZStack(){
+                            CalendarVLinesView(
                                 hourLabel: hourLabel,
                                 dayWidth: dayWidth
                             )
+                            ForEach(eventViewModel.processedEvents, id: \.self){ events in
+                                ForEach(events){ event in
+                                    CalendarEventView(
+                                        event: event,
+                                        startHour: startHour,
+                                        endHour: endHour,
+                                        calendarHeight: calendarHeight,
+                                        hourLabel: hourLabel,
+                                        dayWidth: dayWidth
+                                    )
+                                }
+                            }
                         }
                     }
                 }
-                .offset(x: hourLabel.width + 10)
             }
         }
     }
